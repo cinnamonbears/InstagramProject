39af447156c5af1d46f6146f2675e75e2b61cfa1
AxiosAmnesia <tannan1964@hotmail.com>
2015-11-08 18:36:29 -0700

    converted Home page

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-
   res.render('home', {
-    title: 'This is the Home Page',
+    title: 'Home',
     welcome: 'Welcome to the site!'
   })
 })
