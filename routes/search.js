a68563a75465c649835c33f13f81879b299512aa
zrasmuson <zrasmuson@gmail.com>
2015-11-13 12:36:59 -0700

    installed body-parser

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 router.get('/', function(req, res) {
 	res.render('search', {
 		title: 'Search',
