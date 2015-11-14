91865a8ebf2b4929b28e167f4af4086ef5686dc4
Logan <snowpunx@gmail.com>
2015-11-14 01:25:28 -0700

    Adding the if statement to not display if no search

--- routes/search.js
+++ routes/search.js
@@ -3,32 +3,41 @@ var router = express.Router();
 var request = require('request')
 var bodyParser = require('body-parser')
 
-var query
+var query = "";
 
 router.post('/', function(req, res){
 	query = req.body.search
 	res.redirect('search')
+	console.log('Post Page: ' + query)
 })
 
 router.get('/', function(req, res) {
+	console.log('Search Page: ' +query)
 	var options = {
 		url: 'https://api.instagram.com/v1/tags/' + query + '/media/recent?access_token=' + req.session.access_token
 	}
-	request.get(options, function(error, response, body){
-		try{
-			var feed = JSON.parse(body)
-				if(feed.meta.code > 200){
-					return next(feed.meta.error_message)
+	if(query != ""){
+		request.get(options, function(error, response, body){
+			try{
+				var feed = JSON.parse(body)
+					if(feed.meta.code > 200){
+						return next(feed.meta.error_message)
+					}
+				}catch(err){
+					return next(err)
 				}
-			}catch(err){
-				return next(err)
-			}
-			res.render('search', {
-				title: 'Search',
-				layout: 'auth_base',
-				feed: feed.data
+				res.render('search', {
+					title: 'Search',
+					layout: 'auth_base',
+					feed: feed.data
+				})
 			})
-	})
-	})
+	}else{
+		res.render('search', {
+			title: 'Search',
+			layout: 'auth_base'
+		})
+	}
+})
 
 module.exports = router
